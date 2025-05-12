20c35b7a58883c505b0846f5f9495cc0560641b9
rajeev0521 <rajeev.382761@gmail.com>
2025-05-13 03:36:16 +0530

    updated missing value

--- Array/missing_value.cpp
+++ Array/missing_value.cpp
@@ -16,6 +16,25 @@ int missing_num(vector<int> arr, int n){
     //space complexity: O(1)
 }
 
+int missing_num2(vector<int> arr, int n){
+    int N = n-1;
+    int xor1 = 0, xor2 = 0;
+    for(int i = 0; i < N; i++){
+        xor2 = xor2 ^ arr[i];
+        xor1 = xor1 ^ (i + 1);
+    }
+    xor1 = xor1 ^ n;
+    return xor1 ^ xor2;
+    //Time complexity: O(n)
+    //space complexity: O(1)
+}
+
+/*
+why xox is better than sum = n*(n+1)/2?
+If n is large, n * (n + 1) may overflow
+example: for n = 100,000 → n*(n+1)/2 = 5,000,050,000, which exceeds 32-bit int limit.
+*/
+
 int main(){
     int n;
     cout <<"Enter the size of array: ";
@@ -33,4 +52,7 @@ int main(){
     int result = missing_num(arr,n);
 
     cout <<" the missing number is: " << result << endl;
+
+    result = missing_num2(arr,n);
+    cout << "the missing number is: " << result <<endl;
 }
